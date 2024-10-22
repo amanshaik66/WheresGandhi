2cfdef31e608bccfa15537dab79caae0f5524efb
amanshaik66 <65964478+amanshaik66@users.noreply.github.com>
2024-10-22 09:14:03 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
+// Firebase Configuration
+  const firebaseConfig = {
 
+    apiKey: "AIzaSyBHAFsCc4lq4BceInquAKscpNvX988i4_Q",
+
+    authDomain: "wheresgandhi-77451.firebaseapp.com",
+
+    projectId: "wheresgandhi-77451",
+
+    storageBucket: "wheresgandhi-77451.appspot.com",
+
+    messagingSenderId: "174362976106",
+
+    appId: "1:174362976106:web:6b86a71d0b679f4ebe0f13",
+
+    measurementId: "G-Z5QFTF0V58"
+
+  };
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const form = document.getElementById('signup-form');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    console.log(`Email submitted: ${email}`);
+});
+
+// Initialize OpenStreetMap
+const map = L.map('map').setView([20.5937, 78.9629], 5); // Centered on India
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
