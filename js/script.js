eb3acb09154cc8c7335e4cce39cfcac341677cb1
amanshaik66 <65964478+amanshaik66@users.noreply.github.com>
2024-10-22 10:53:16 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -10,8 +10,8 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
+// import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js";
+// import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-analytics.js";
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
